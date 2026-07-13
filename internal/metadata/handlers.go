1e33c800a58da35f5676a6ee88566140918f48b7
advaithdp02 <advaithdp@gmail.com>
2026-07-13 12:46:07 +0530

    feat: implement HTTP API handlers
    Wired upload, download, list, stat, and delete endpoints to
    actual storage methods. Upload streams to temp file then stores;
    download serves file with Content-Disposition header.

--- internal/metadata/handlers.go
+++ internal/metadata/handlers.go
@@ -1,8 +1,12 @@
-//Package metadata is used for metadata server actions
+// Package metadata is used for metadata server actions
 package metadata
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/Advaithdp02/s3lite/internal/storage"
 )
@@ -14,53 +18,113 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 
 func UploadHandler(store *storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
 
-		// TODO:
-		// Read uploaded file
-		// Save temporarily
-		// store.Upload(...)
+		tmp, err := os.CreateTemp("", "upload-*")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer os.Remove(tmp.Name())
 
-		w.Write([]byte("Upload endpoint"))
+		io.Copy(tmp, file)
+		tmp.Close()
+
+		finalPath := filepath.Join(filepath.Dir(tmp.Name()), header.Filename)
+		os.Rename(tmp.Name(), finalPath)
+		defer os.Remove(finalPath)
+
+		if err := store.Upload(finalPath); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte("uploaded"))
 	}
 }
 
 func DownloadHandler(store *storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Query().Get("file")
+		if filename == "" {
+			http.Error(w, "missing file", http.StatusBadRequest)
+			return
+		}
 
-		// TODO:
-		// Read filename
-		// store.Download(...)
+		tmpDir, err := os.MkdirTemp("", "download-*")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer os.RemoveAll(tmpDir)
 
-		w.Write([]byte("Download endpoint"))
+		if err := store.Download(filename, tmpDir); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		path := filepath.Join(tmpDir, filename)
+
+		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+		http.ServeFile(w, r, path)
 	}
 }
 
 func ListHandler(store *storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		objects, err := store.List()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-		// TODO:
-		// objects := store.List()
+		w.Header().Set("Content-Type", "application/json")
 
-		w.Write([]byte("List endpoint"))
+		if err := json.NewEncoder(w).Encode(objects); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
 func StatHandler(store *storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Query().Get("file")
+		if filename == "" {
+			http.Error(w, "missing file parameter", http.StatusBadRequest)
+			return
+		}
 
-		// TODO:
-		// store.Stat()
+		meta, err := store.Stat(filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(meta)
 		w.Write([]byte("Stat endpoint"))
 	}
 }
 
 func DeleteHandler(store *storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Query().Get("file")
+		if filename == "" {
+			http.Error(w, "missing file parameter", http.StatusBadRequest)
+			return
+		}
 
-		// TODO:
-		// store.Delete()
+		if err := store.Delete(filename); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-		w.Write([]byte("Delete endpoint"))
+		w.Write([]byte("deleted"))
 	}
 }
